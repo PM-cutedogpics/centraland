4a4a95e6bb8b2220b90dc03d4220a7d39fb29d89
jmaglaque <jose_maglaqueiii@dlsu.edu.ph>
2023-07-03 22:41:30 +0800

    Header updates

--- src/components/WebsiteHeader.tsx
+++ src/components/WebsiteHeader.tsx
@@ -1,9 +1,35 @@
-import { Header } from "@mantine/core";
+/* eslint-disable react/jsx-no-undef */
+import { Avatar, Group, Header, Title, Text } from "@mantine/core";
 
 // TODO: Do header
 const WebsiteHeader = () => {
     return (  
-        <Header>Test</Header>
+        <Header height={60} p="xs" >
+
+            <Group position="apart" align="center" sx={{
+                    fontFamily: "Montserrat !important"
+                }}>
+
+                <Group>
+                    <Avatar></Avatar>
+                    <Text   size={30} 
+                            weight={700}>
+                            
+                        CentralandPH
+                        </Text>
+                </Group>
+
+
+                <Group >
+                    <Text weight={700}>All</Text>
+                    <Text weight={700}>Games</Text>
+                    <Text weight={700}>Consoles</Text>
+                    <Text weight={700}>PC</Text>
+                    <Text weight={700}>Peripherals</Text>
+                    <Avatar></Avatar>
+                </Group>
+            </Group>
+        </Header>
     );
 }
  
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,9 @@ export default class _Document extends Document {
       <Html>
         <Head />
         <body>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&display=swap" rel="stylesheet"/>
           <Main />
           <NextScript />
         </body>
