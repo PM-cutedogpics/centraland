4f420cb80d6eb90cffc6f121159598afc6374e35
Ben <benedict_sun@dlsu.edu.ph>
2023-07-06 01:16:40 +0800

    Setup product item component
    Yet to setup props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head"
-
+import ProductItem from "../components/ProductItem/ProductItem";
 
 export default function IndexPage() {
     return ( 
@@ -7,7 +7,29 @@ export default function IndexPage() {
         <Head>
             <title>Centraland</title>
         </Head>
-        <p className="bg-red-600 text-4xl">Hello</p>
+      
+        {/* What's Hot Section */}
+        <section className="container mx-auto">
+            <div className="flex flex-col gap-3 bg-gray-200 px-20">
+                <h2 className="section-header">What&apos;s Hot?</h2>
+                
+                <div className="flex gap-3">
+
+                </div>
+                
+                <div className="flex gap-4">
+                    <ProductItem />
+                    <ProductItem />
+                    <ProductItem />
+                    {/* <ProductItem /> */}
+                </div>
+            </div>            
+        </section>
+
+        {/* Newly Listed */}
+        
+        {/* For You */}
+        
     </> 
     );
 }
