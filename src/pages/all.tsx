d0d9f195640d6369352b1df28a096146727bdf0e
J3Jab <jason_jan_jabanes@dlsu.edu.ph>
2023-07-07 23:47:52 +0800

    implemented sidebar

--- src/pages/all.tsx
+++ src/pages/all.tsx
@@ -1,14 +1,34 @@
 import Head from "next/head"
-
+import { AppShell, MantineProvider, Aside } from '@mantine/core';
+import Sidebar from "@/components/Sidebar/Sidebar";
+import ProductItem from "../components/ProductItem/ProductItem";
 export default function All() {
+    
     return ( 
     <>
         <Head>
-            <title>Shop</title>
+            <title>CentralandPH</title>
         </Head>
-        <section>
-            All Page
+
+        <AppShell aside={<Sidebar />}>
+
+        <section className="container mx-auto absolute left-60 top-[60px]">
+                
+                <div className="flex gap-3">
+                </div>
+                
+                <div className="flex gap-4">
+                    <ProductItem />
+                    <ProductItem />
+                    <ProductItem />
+                    {/* <ProductItem /> */}
+                </div>         
         </section>
+        </AppShell>
+
+
+
+
     </> 
     );
 }
