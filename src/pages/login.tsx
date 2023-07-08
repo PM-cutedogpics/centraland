115c0b525daae52545c6766525731932827a276f
John Matthew Gan <24535419+ganmatthew@users.noreply.github.com>
2023-07-08 22:56:28 +0800

    Revert "Origin/feature login register"

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -14,6 +14,8 @@ export default function Login() {
             <title>Login</title>
         </Head>
         <section>
+            <h1>Signin Page</h1>
+            <LoginModal /> {/* Render the LoginModal component */}
         </section>
     </> 
     );
