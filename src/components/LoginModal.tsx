cae91ad43eb6a90a2a07ff56dc6c40ac861833bb
jmaglaque <jose_maglaqueiii@dlsu.edu.ph>
2023-07-09 16:11:52 +0800

    updated login modal

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -1,19 +1,17 @@
-import { useState, ChangeEvent, FormEvent } from 'react';
-import '../app/globals.css';
+import { useState, ChangeEvent, FormEvent } from "react";
+import "../app/globals.css";
+import { Container, Modal, Space } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 
 interface LoginModalProps {
-    onClose: Function;
+  onClose: Function;
+  opened: any;
+  close: any;
 }
 
 const LoginModal = (props: LoginModalProps) => {
-  const [isOpen, setIsOpen] = useState(true); // Update the initial state to true
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleCloseModal = () => {
-    props.onClose();
-    setIsOpen(false);
-  };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
@@ -30,76 +28,58 @@ const LoginModal = (props: LoginModalProps) => {
     // You can use the email and password variables
 
     // Clear form fields
-    setEmail('');
-    setPassword('');
+    setEmail("");
+    setPassword("");
   };
 
   return (
-    <div>
-      {isOpen && (
-        <div className='modal'>
-            <div className="modal-content">
-            <div className="absolute inset-0 bg-gray-900 opacity-75"></div>
-            <div className="bg-white rounded-lg p-8 relative z-10">
-                <img src='/cph-logo.png' className='company-icon'></img>
-                <div className="absolute top-3 right-3">
-                <button
-                    className="bg-transparent text-gray-500 hover:text-gray-700 font-bold text-3xl leading-none outline-none focus:outline-none"
-                    onClick={handleCloseModal}
-                >
-                    <span>&times;</span>
-                </button>
-                </div>
-                <h1 className="greeting">Login</h1>
-                <form onSubmit={handleSubmit}>
-                <div className="mb-4">
-                    <div className="label" >
-                    <label>
-                        Email
-                    </label>
-                    </div>
-                    <input
-                    className="inputSign" // Replace with the appropriate class name from your globals.css file
-                    id="email"
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={handleEmailChange}
-                    />
-                </div>
-                <div className="mb-6">
-                    <div className="label" >
-                    <label>
-                        Password
-                    </label>
-                    </div>
-                    <input
-                    className="inputSign" // Replace with the appropriate class name from your globals.css file
-                    id="password"
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={handlePasswordChange}
-                    />
-                </div>
-                <div className="label">
-                    <button
-                    className="button" // Replace with the appropriate class name from your globals.css file
-                    type="submit"
-                    >
-                    Log in
-                    </button>
-                </div>
-                </form>
-                <div className="text-center">
-                New to CentralandPH?
-                <a href='signup'> Join Now</a>
-                </div>
-            </div>
-            </div>
+    <Modal opened={props.opened} onClose={props.close} radius="md" size="lg">
+      <img src="/cph-logo.png" className="company-icon"></img>
+      <h1 className="greeting">Login</h1>
+
+      <Space h="xl" />
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <div className="label">
+            <label>Email</label>
+          </div>
+          <input
+            className="inputSign" // Replace with the appropriate class name from your globals.css file
+            id="email"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={handleEmailChange}
+          />
         </div>
-      )}
-    </div>
+        <div className="mb-6">
+          <div className="label">
+            <label>Password</label>
+          </div>
+          <input
+            className="inputSign" // Replace with the appropriate class name from your globals.css file
+            id="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <Container w={510}>
+          <button
+            className="button" // Replace with the appropriate class name from your globals.css file
+            type="submit"
+          >
+            Log in
+          </button>
+        </Container>
+      </form>
+      <div className="text-center">
+        New to CentralandPH?
+        <a href="signup"> Join Now</a>
+      </div>
+      <Space h={100} />
+    </Modal>
   );
 };
 
--- src/components/WebsiteNavbar.tsx
+++ src/components/WebsiteNavbar.tsx
@@ -1,13 +1,16 @@
 /* eslint-disable react/jsx-no-undef */
 import { Avatar, Group, Header, Title, Text } from "@mantine/core";
-import Image from 'next/image'
-import Link from 'next/link';
-import React, { useState } from 'react';
-import LoginModal from '@/components/LoginModal';
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import LoginModal from "@/components/LoginModal";
+import { useDisclosure } from "@mantine/hooks";
 
 const WebsiteNavbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+
+  const [opened, { open, close }] = useDisclosure(false); // Handling ng pagopen and close ng modal
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
@@ -21,9 +24,14 @@ const WebsiteNavbar = () => {
     setIsLoginModalOpen(false);
   };
 
-  return (  
-    <Header height={60} p="xs">               
-      <Group position="apart" align="center" sx={{ fontFamily: "Montserrat !important" }}>
+  return (
+    <Header height={60} p="xs">
+      <LoginModal onClose={closeLoginModal} opened={opened} close={close} />
+      <Group
+        position="apart"
+        align="center"
+        sx={{ fontFamily: "Montserrat !important" }}
+      >
         <Group>
           <Link href="/">
             <Image
@@ -36,13 +44,23 @@ const WebsiteNavbar = () => {
         </Group>
 
         <Group>
-          <Link className="nav-item" href="all"><Text weight={700}>All</Text></Link>
-          <Link className="nav-item" href="games"><Text weight={700}>Games</Text></Link>
-          <Link className="nav-item" href="consoles"><Text weight={700}>Consoles</Text></Link>
-          <Link className="nav-item" href="pc"><Text weight={700}>PC</Text></Link>
-          <Link className="nav-item" href="peripherals"><Text weight={700}>Peripherals</Text></Link>
+          <Link className="nav-item" href="all">
+            <Text weight={700}>All</Text>
+          </Link>
+          <Link className="nav-item" href="games">
+            <Text weight={700}>Games</Text>
+          </Link>
+          <Link className="nav-item" href="consoles">
+            <Text weight={700}>Consoles</Text>
+          </Link>
+          <Link className="nav-item" href="pc">
+            <Text weight={700}>PC</Text>
+          </Link>
+          <Link className="nav-item" href="peripherals">
+            <Text weight={700}>Peripherals</Text>
+          </Link>
 
-          <div style={{ position: 'relative' }}>
+          <div style={{ position: "relative" }}>
             <Image
               width={30}
               height={30}
@@ -54,7 +72,9 @@ const WebsiteNavbar = () => {
             {isOpen && (
               <ul className="dropdown-menu">
                 <li>
-                  <button className="button" onClick={openLoginModal}>Login</button>
+                  <button className="button" onClick={open}>
+                    Login
+                  </button>
                 </li>
                 <li>
                   <a href="signup">Signup</a>
@@ -64,9 +84,6 @@ const WebsiteNavbar = () => {
           </div>
         </Group>
       </Group>
-      {isLoginModalOpen && (
-        <LoginModal onClose={closeLoginModal} />
-      )}
     </Header>
   );
 };
